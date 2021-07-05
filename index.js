11cdc2c38bd4321938e959dac771b890ce9073c9
--Explosion-- <explosion.scratch@gmail.com>
2021-07-04 19:33:16 -0500

    JSDocs documentation

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ class Bot {
 			});
 		});
 	}
+	/**
+	 * Finds a user based on their discord tag, username, nickname or ID.
+	 * @param {Function|String} query The discord tag, find function, username, or user ID of the user to find.
+	 */
 	findUser(query) {
 		console.log(JSON.stringify(this.client.users, null, 2));
 		if (typeof query === "function") {
@@ -50,13 +54,27 @@ class Bot {
 			message: "Please provide a string or a find function.",
 		};
 	}
+	/**
+	 * Sends the user specified a message.
+	 * @param {String|Function} [user=last_user_of_bot] The query to find the user by (same as used in 'findUser'.)
+	 * @param {String|Embed|Object} msg A sendable message that can be send through discord.js
+	 */
 	dm(user, msg) {
 		user = user || this.info.author;
 		this.findUser(user).send(msg);
 	}
+	/**
+	 * Finds a channel based on a query.
+	 * @param {Function} query The find function to find the channel (this command still very much WIP.)
+	 */
 	findChannel(query) {
 		return this.client.channels.cache.find(query);
 	}
+	/**
+	 * Listens for an event emitted through 'bot.dispatch("event", "data")'.
+	 * @param {String} event The event to listen for.
+	 * @param {Function} callback The callback to run when the event happens.
+	 */
 	addListener(event, callback) {
 		this.listeners[event] = this.listeners[event] || [];
 		var id = Math.random();
@@ -68,6 +86,11 @@ class Bot {
 			},
 		};
 	}
+	/**
+	 * Calls all the listeners added by 'addListener' matching the event dispatched. (Note: This function is mostly internal, if you want to use it to manually trigger events though, be my guest!)
+	 * @param {String} event The event to dispatch.
+	 * @param  {...any} data The data to send with the event.
+	 */
 	dispatch(event, ...data) {
 		var listeners = this.listeners[event];
 		if (!listeners) return;
@@ -75,9 +98,13 @@ class Bot {
 			fn.function(...(data || []));
 		}
 	}
+	/**
+	 * Initiates slash commands
+	 */
 	async initSlash() {
 		this.dispatch("init_slash");
-		var guildId = "838149475477618699";
+		// Providing a guildId in config allows slash commands to update immediately. otherwise they take about an hour and update bot wide (all servers).
+		var guildId = this.config.guildId || undefined;
 		var getApp = (guildId) => {
 			const app = this.client.api.applications(this.client.user.id);
 			if (guildId) {
@@ -105,7 +132,6 @@ class Bot {
 			};
 		});
 		commands = JSON.parse(JSON.stringify(commands));
-		// console.log(JSON.stringify(commands, null, 2));
 		for (let command of commands) {
 			var res = await getApp(guildId)
 				.commands.post({ data: command })
@@ -157,6 +183,14 @@ class Bot {
 				});
 		});
 	}
+	/**
+	 * Internal function to run a command in a channel, with a message or interaction. For the most part leave this alone.
+	 * @param {Object} params The params.
+	 * @param {Channel} params.channel The channel that the command was operated in.
+	 * @param {String} params.content The content of the message that was sent.
+	 * @param {Message} params.message A discord.js message object with details about the message sent.
+	 * @param {Interaction} params.interaction A discord.js interaction object with info about the interaction if the event that triggered this command was an interaction.
+	 */
 	runCommand({ channel, content, message, interaction }) {
 		this.dispatch("run_command", { channel, content, message, interaction });
 		for (let item of this.commands) {
@@ -185,9 +219,23 @@ class Bot {
 			}
 		}
 	}
-	addCommand(command, opts) {
+	/**
+	 * Adds a command to the bot.
+	 * @param {Object} command The command arguments.
+	 * @param {Function} command.run The run function for when the command is run by the user. This is passed one argument, which is an object: {message: "The message that the user sent (if not an interaction)", interaction: "The interaction (instead of a message, in the case of slash commands)", args: "An object containing any arguments that got passed to the command when run by the user."}
+	 * The return value of this function is used as the embed properties when sending a response message to the user.
+	 * @param {String} command.title The title of the command (useful for creating a nice help command for your bot. Also used as a default if the run function of the command returns nothing.)
+	 * @param {String} command.description Used as a default if the run function of the command returns nothing.
+	 * @param {Array} [command.args] A list of arguments for the command. Each should be an object containing name, description, and a required boolean. Also can have more options (See discord.js slash commands options documentation).
+	 * @param {Object} command.slashCommand Optional arguments to pass to discord.js slash command creation.
+	 */
+	addCommand(command) {
 		this.commands.push(command);
 	}
+	/**
+	 * Initiates the bot using an authentification token.
+	 * @param {String} token The authentification token used to log the bot in and make it work!
+	 */
 	init(token) {
 		token = token || this.config.BOT_TOKEN;
 		if (!token) {
@@ -198,6 +246,11 @@ class Bot {
 		this.client.login(token);
 		return this;
 	}
+	/**
+	 * Sets the bot's prefix for use in message based commands (E.g. '#help').
+	 * Does not affect slash commands.
+	 * @param {String} prefix The new prefix for the bot.
+	 */
 	setPrefix(prefix) {
 		if (!prefix) {
 			throw new Error(
@@ -207,12 +260,23 @@ class Bot {
 		this.config.prefix = prefix;
 		return this;
 	}
+	/**
+	 * Sets a property in the bot's config to a value.
+	 * @param {String} prop The property to set.
+	 * Current config properties are: color, prefix, and guildId
+	 * @param {any} val The value to set the property to.
+	 */
 	set(prop, val) {
 		if (!(prop && val)) {
 			throw new Error("Prop and val are required.");
 		}
 		this.config[prop] = val;
+		return this;
 	}
+	/**
+	 * Internal function to create an embed object to send in a message from an opts object/string
+	 * @param {String|Object} opts The options object.
+	 */
 	embed(opts) {
 		if (typeof opts === "string") {
 			return { embed: { description: opts } };
